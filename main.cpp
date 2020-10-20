e924b541c42b8ff576575cba46493cbf0d144ebf
barto <gb45644@zut.edu.pl>
2020-10-20 21:06:05 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+int main() {
+
+    int N;
+    float f;
+
+    N = rand() % 10000 + -1000;
+    
+
+    return 0;
+}
