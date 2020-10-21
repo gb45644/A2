9ef68dd3410c838ec640a50d7039145b94d7dfac
barto <gb45644@zut.edu.pl>
2020-10-21 22:32:27 +0200

    load txt nie działa :( tfu cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
-#include <stdlib.h>
-#include <cstdlib>
-
+#include <fstream>
+#include <string>
 using namespace std;
 
 
 
 int main() {
+ifstream loadfile;
 
-    int N;
-    float f;
-
-    N = rand() % 10000 + -1000;
-    
-
+struct lab1
+    {
+    int first;
+    char second;
+    float third;
+    };
+loadfile.open("inlab01.txt");
+    if (loadfile.fail()) {
+        cout << "fail";
+    }
+    else{
+        string description;
+        while (loadfile>>description)
+            {
+            cout << description << endl;
+            }
+    }
     return 0;
 }
